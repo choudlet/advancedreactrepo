eb605a008f786e50e08a227dd03be94ba91d8aef
Christopher Houdlette <christopher@choozle.com>
2018-12-09 21:01:14 -0700

    checking if logged in on the createItem mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,10 +6,20 @@ const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: check if the yare logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged into do that!");
+    }
+
     const item = await ctx.db.mutation.createItem(
       {
-        data: { ...args }
+        data: {
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
+          ...args
+        }
       },
       info
     );
