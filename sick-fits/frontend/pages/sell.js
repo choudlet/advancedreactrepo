dbcc8bb21e3e826eee3b51eff5dadf0500d6e55d
Christopher Houdlette <christopher@choozle.com>
2018-12-10 08:54:20 -0700

    got the nice wrapper component working well

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,12 @@
 import CreateItem from "../components/CreateItem";
+import SignInReminder from "../components/SignInReminder";
+import SignIn from "../components/RequestReset";
 
 const Sell = props => (
   <div>
-    <CreateItem />
+    <SignInReminder>
+      <CreateItem />
+    </SignInReminder>
   </div>
 );
 
