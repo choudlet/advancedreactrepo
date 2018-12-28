fb969e92e6343b0cf0d5282cde4f3e133b25bfb6
Christopher Houdlette <christopher@choozle.com>
2018-12-28 13:22:28 -0500

    fixing the cart stuff

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,22 +1,49 @@
 import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
 const Cart = () => {
   return (
-    <CartStyles open={true}>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have __ items in your cart</p>
-      </header>
-      <footer>
-        <p>10.10</p>
-      </footer>
-    </CartStyles>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({ data, error, loading }) => {
+            return (
+              <CartStyles open={data.cartOpen}>
+                <header>
+                  <CloseButton onClick={toggleCart} title="close">
+                    &times;
+                  </CloseButton>
+                  <Supreme>Your Cart</Supreme>
+                  <p>You have __ items in your cart</p>
+                </header>
+                <footer>
+                  <p>10.10</p>
+                </footer>
+              </CartStyles>
+            );
+          }}
+        </Query>
+      )}
+    </Mutation>
   );
 };
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
